054747e5a298a8832c6829ea81dc5de47a27d002
ricardotq182 <ricardocb1191@gmail.com>
2016-08-04 12:37:28 -0500

    Create hola.php

---
+++ hola.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>Mi quinta página con php</title>
+  </head>
+  <body>
+    <h1>Mi quinto ejemplo Php</h1>
+    <p>Este es mi quinto ejemplo con partes de Php</p>
+
+	<?php
+echo "probando";
+	?>
+
+  </body>
+</html>
